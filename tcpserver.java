43a36f19b36dc66dad9ecf53ca405604b3fbdadc
SILPAMANOJ <silpapalassery@gmail.com>
2018-03-27 09:29:52 +0530

    EchoServer and EchoClient programs

--- tcpserver.java
+++ tcpserver.java
@@ -2,7 +2,7 @@
     import java.io.*;  
     class tcpserver{  
     public static void main(String args[])throws Exception{  
-    ServerSocket ss=new ServerSocket(3334);  
+    ServerSocket ss=new ServerSocket(3333);  
     Socket s=ss.accept();  
     DataInputStream din=new DataInputStream(s.getInputStream());  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
